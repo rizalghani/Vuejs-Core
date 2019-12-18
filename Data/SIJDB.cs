14fcc26c9639abf0511a037136d9e03970287f2d
Mohammad Rizal Ghani <rizal.pnd.work@gmail.com>
2019-12-18 11:47:13 +0700

    update(2)

---
+++ Data/SIJDB.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SIJ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIJ.Data
+{
+    public class SIJDB : DbContext
+    {
+        public SIJDB(DbContextOptions<SIJDB> options) : base(options)
+        {
+        }
+
+        public DbSet<Bug> Bugs { get; set; }
+    }
+}
